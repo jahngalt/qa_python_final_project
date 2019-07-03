99bea98ba1f1214de4d499a3c47e66d0bbe1e4d4
Oleg Kudimov <ovkudimov@gmail.com>
2019-07-03 11:19:45 +0300

    rename name of test method

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,9 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+link = "http://selenium1py.pythonanywhere.com/"
+
+def go_to_login_page(browser):
+    link = browser.find_element_by_css_selector("#login_link")
+    link.click()
+
+def test_check_add_button_on_the_page_with_differnt_languages(browser):
     browser.get(link)
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+    go_to_login_page(browser)
